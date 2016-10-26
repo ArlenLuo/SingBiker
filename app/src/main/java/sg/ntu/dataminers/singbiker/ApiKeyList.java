bda45148cf72ba19cd21cf742c5757a23c557508
Viktor Sieger <viktorsieger@users.noreply.github.com>
2016-10-26 16:15:34 +0800

    Initial commit

---
+++ app/src/main/java/sg/ntu/dataminers/singbiker/ApiKeyList.java
@@ -0,0 +1,7 @@
+package sg.ntu.dataminers.singbiker;
+
+public final class ApiKeyList {
+	public static final String LTA_DATAMALL="JVP1zooGTj6MGJjBJWGc/Q==";
+	public static final String DATA_GOV="xpkGjYVymZ0h0M15kygAwl3MYmpmBy6b";
+	public static final String GOOGLE_MAPS=" AIzaSyBxMXbNMa3ckf8EDX6L99UMgnttaLQiGzc";
+}
